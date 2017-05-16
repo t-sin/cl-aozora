9ccb11c25860f58251517c7fb3593c40065d1b0b
t-sin <shinichi.tanaka45@gmail.com>
2017-05-16 23:47:33 +0900

    Adds aozora special characters

--- main.lisp
+++ main.lisp
@@ -4,3 +4,16 @@
   (:use :cl))
 (in-package :cl-aozora/main)
 
+;;; aozora special characters
+(defvar +fw-square-bracket-left+ #.(char "［］" 0))
+(defvar +fw-square-bracket-right+ #.(char "［］" 1))
+
+(defvar +fw-double-angle-bracket-left+ #.(char "《》" 0))
+(defvar +fw-double-angle-bracket-right+ #.(char "《》" 1))
+
+(defvar +fw-turtoise-shell-bracket-left+ #.(char "〔〕" 0))
+(defvar +fw-turtoise-shell-bracket-right+ #.(char "〔〕" 1))
+
+(defvar +fw-vertical-line+ #.(char "｜" 0))
+(defvar +fw-number-sign+ #.(char "＃" 0))
+(defvar +fw-reference-mark+ (char "※" 0))
